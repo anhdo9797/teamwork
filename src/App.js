5efa4615bf590f1f6cadf0f6c936ae49e1b8f9ef
anhdo9797 <docongphuocanh.2697@gmail.com>
2020-08-27 21:22:51 +0700

    backup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import 'antd/dist/antd.css';
 import Home from './pages/Home/index.jsx';
 
 function App() {
-    return (
-        <Router>
-            <Home />
-        </Router>
-    );
+  return (
+    <Router>
+      <Home />
+    </Router>
+  );
 }
 
 export default App;
--- src/assets/icon/index.js
+++ src/assets/icon/index.js
@@ -1,3 +1,7 @@
-import fb from "./facebook.png";
-import logo from "./logo.png";
-export { fb, logo };
+import fb from './facebook.png';
+import logo from './logo.png';
+import ux from './ux.png';
+import dev from './dev.png';
+import branch from './brand.png';
+
+export { fb, logo, ux, dev, branch };
