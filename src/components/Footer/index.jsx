5e8bc01b1ae6042d8f2d068061c498d811df2dab
Man Man <chethiminhman121998@gmail.com>
2020-08-31 09:20:04 +0700

    Fix code in file index footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,72 +3,72 @@ import './style.scss';
 import 'antd/dist/antd.css';
 
 class Footer extends React.Component {
-    render() {
-        return (
-            <div>
-                <section id="footer">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-4">
-                                <h5>Offices</h5>
-                                <div>
-                                    50 Raritan Center Pkwy, <br />
-                                    Edison, NJ 08837, USA
-                                </div>
-                                <div style={{ display: 'flex' }}>
-                                    <div>
-                                        <ion-icon style={{ color: '#FF268B', marginRight: '10px' }} name="call-outline"></ion-icon>
-                                    </div>
-                                    <div>8080000000</div>
-                                </div>
-                                <div style={{ display: 'flex' }}>
-                                    <div>
-                                        <ion-icon style={{ color: '#FF268B', marginRight: '10px' }} name="mail-outline"></ion-icon>
-                                    </div>
-                                    <div>8080000000</div>
-                                </div>
-                                <div>
-                                    <span className="icon-media">
-                                        <ion-icon style={{ color: '#FF268B', marginRight: '10px', fontSize: '20px' }} name="logo-facebook"></ion-icon>
-                                    </span>
-                                    <span className="icon-media">
-                                        <ion-icon style={{ color: '#FF268B', marginRight: '10px', fontSize: '20px' }} name="logo-instagram"></ion-icon>
-                                    </span>
-                                    <span className="icon-media">
-                                        <ion-icon style={{ color: '#FF268B', marginRight: '10px', fontSize: '20px' }} name="logo-twitter"></ion-icon>
-                                    </span>
-                                </div>
-                            </div>
-                            <div className="col-4">
-                                <h5>Service</h5>
-                                <div>Portfolio</div>
-                                <div>About</div>
-                                <div>Marketplace</div>
-                                <div>Experts</div>
-                                <div>Blog</div>
-                            </div>
-                            <div className="col-4">
-                                <h5>Popular</h5>
-                                <div>Free videos</div>
-                                <div>Tools, apps and plugins</div>
-                                <div>Blog</div>
-                                <div>Photo license</div>
-                                <div>FAQ</div>
-                                <div>API</div>
-                            </div>
-                            <div className="col-4">
-                                <h5>Service</h5>
-                                <div>Training</div>
-                                <div>Marketplace</div>
-                                <div>Experts</div>
-                                <div>Guides</div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
+  render() {
+    return (
+      <div>
+        <section id="footer">
+          <div className="container">
+            <div className="row">
+              <div className="col-4">
+                <h5>Offices</h5>
+                <div>
+                  50 Raritan Center Pkwy, <br />
+                  Edison, NJ 08837, USA
+                </div>
+                <div style={{ display: 'flex' }}>
+                  <div>
+                    <ion-icon style={{ color: '#FF268B', marginRight: '10px' }} name="call-outline"></ion-icon>
+                  </div>
+                  <div>8080000000</div>
+                </div>
+                <div style={{ display: 'flex' }}>
+                  <div>
+                    <ion-icon style={{ color: '#FF268B', marginRight: '10px' }} name="mail-outline"></ion-icon>
+                  </div>
+                  <div>8080000000</div>
+                </div>
+                <div>
+                  <span className="icon-media">
+                    <ion-icon style={{ color: '#FF268B', marginRight: '10px', fontSize: '20px' }} name="logo-facebook"></ion-icon>
+                  </span>
+                  <span className="icon-media">
+                    <ion-icon style={{ color: '#FF268B', marginRight: '10px', fontSize: '20px' }} name="logo-instagram"></ion-icon>
+                  </span>
+                  <span className="icon-media">
+                    <ion-icon style={{ color: '#FF268B', marginRight: '10px', fontSize: '20px' }} name="logo-twitter"></ion-icon>
+                  </span>
+                </div>
+              </div>
+              <div className="col-4">
+                <h5>Service</h5>
+                <div>Portfolio</div>
+                <div>About</div>
+                <div>Marketplace</div>
+                <div>Experts</div>
+                <div>Blog</div>
+              </div>
+              <div className="col-4">
+                <h5>Popular</h5>
+                <div>Free videos</div>
+                <div>Tools, apps and plugins</div>
+                <div>Blog</div>
+                <div>Photo license</div>
+                <div>FAQ</div>
+                <div>API</div>
+              </div>
+              <div className="col-4">
+                <h5>Service</h5>
+                <div>Training</div>
+                <div>Marketplace</div>
+                <div>Experts</div>
+                <div>Guides</div>
+              </div>
             </div>
-        );
-    }
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Footer;
