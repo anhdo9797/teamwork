0ca06ec990d7192f7f027834f5c659dceee4d2b7
Man Man <chethiminhman121998@gmail.com>
2020-08-31 16:26:41 +0700

    commit code before merge to dev-man

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,68 +3,6 @@ import './style.scss';
 import 'antd/dist/antd.css';
 
 class Footer extends React.Component {
-<<<<<<< HEAD
-  render() {
-    return (
-      <div>
-        <section id="footer">
-          <div className="container">
-            <div className="row">
-              <div className="col-4">
-                <h5>Offices</h5>
-                <div>
-                  50 Raritan Center Pkwy, <br />
-                  Edison, NJ 08837, USA
-                </div>
-                <div style={{ display: 'flex' }}>
-                  <div>
-                    <ion-icon style={{ color: '#FF268B', marginRight: '10px' }} name="call-outline"></ion-icon>
-                  </div>
-                  <div>8080000000</div>
-                </div>
-                <div style={{ display: 'flex' }}>
-                  <div>
-                    <ion-icon style={{ color: '#FF268B', marginRight: '10px' }} name="mail-outline"></ion-icon>
-                  </div>
-                  <div>8080000000</div>
-                </div>
-                <div>
-                  <span className="icon-media">
-                    <ion-icon style={{ color: '#FF268B', marginRight: '10px', fontSize: '20px' }} name="logo-facebook"></ion-icon>
-                  </span>
-                  <span className="icon-media">
-                    <ion-icon style={{ color: '#FF268B', marginRight: '10px', fontSize: '20px' }} name="logo-instagram"></ion-icon>
-                  </span>
-                  <span className="icon-media">
-                    <ion-icon style={{ color: '#FF268B', marginRight: '10px', fontSize: '20px' }} name="logo-twitter"></ion-icon>
-                  </span>
-                </div>
-              </div>
-              <div className="col-4">
-                <h5>Service</h5>
-                <div>Portfolio</div>
-                <div>About</div>
-                <div>Marketplace</div>
-                <div>Experts</div>
-                <div>Blog</div>
-              </div>
-              <div className="col-4">
-                <h5>Popular</h5>
-                <div>Free videos</div>
-                <div>Tools, apps and plugins</div>
-                <div>Blog</div>
-                <div>Photo license</div>
-                <div>FAQ</div>
-                <div>API</div>
-              </div>
-              <div className="col-4">
-                <h5>Service</h5>
-                <div>Training</div>
-                <div>Marketplace</div>
-                <div>Experts</div>
-                <div>Guides</div>
-              </div>
-=======
     render() {
         return (
             <div>
@@ -150,13 +88,8 @@ class Footer extends React.Component {
                         </div>
                     </div>
                 </section>
->>>>>>> 913d2dfe6082ae27d194918d8b1bc4568b4598d6
             </div>
-          </div>
-        </section>
-      </div>
-    );
-  }
+        );
+    }
 }
-
 export default Footer;
