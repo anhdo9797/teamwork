b701f0fed4f180b420c7875f7ee6f0450c5fcccc
Man Man <chethiminhman121998@gmail.com>
2020-08-31 08:38:38 +0700

    backup

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,17 +1,17 @@
 'use strict';
 module.exports = {
-    singleQuote: true,
-    trailingComma: 'all',
-    printWidth: 810,
-    tabWidth: 2,
-    proseWrap: 'never',
-    endOfLine: 'auto',
-    overrides: [
-        {
-            files: '.prettierrc',
-            options: {
-                parser: 'json',
-            },
-        },
-    ],
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 810,
+  tabWidth: 2,
+  proseWrap: 'never',
+  endOfLine: 'auto',
+  overrides: [
+    {
+      files: '.prettierrc',
+      options: {
+        parser: 'json',
+      },
+    },
+  ],
 };
