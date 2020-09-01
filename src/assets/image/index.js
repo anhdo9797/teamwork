de9cb1fb06a5f8c1306180ca0fc7af20c9fd8990
Man Man <chethiminhman121998@gmail.com>
2020-09-01 16:17:32 +0700

    Fix lint erro

--- src/assets/image/index.js
+++ src/assets/image/index.js
@@ -21,25 +21,6 @@ import pattern from './pattern.png';
 import videoStudio from './videoStudio.png';
 import lineHeader from './lineHeader.png';
 
-
 import patternHeader from './patternHeader.png';
 
-export {
-    patternHeader,
-    lineHeader,
-    videoStudio,
-    circle,
-    pattern,
-    donors,
-    build1,
-    build2,
-    build3,
-    build4,
-    card1,
-    card2,
-    card3,
-    line,
-    services,
-    girl,
-};
-
+export { patternHeader, lineHeader, videoStudio, circle, pattern, donors, build1, build2, build3, build4, card1, card2, card3, line, services, girl };
